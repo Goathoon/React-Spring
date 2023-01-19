cb3f983b4a7743100cff7e004c945ca7a78b6047
DESKTOP-MIDQG7C\태훈 <ownest2@gmail.com>
2023-01-19 15:31:17 +0900

    연동 전 자잘한 실수 수정중

--- SpringAndReact/frontend/src/containers/RegisterForm.js
+++ SpringAndReact/frontend/src/containers/RegisterForm.js
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm, register } from '../modules/auth';
 import AuthForm from '../components/auth/AuthForm';
 
 const RegisterForm = () => {
-  const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
+  const dispatch = useDispatch(); //스토어에서 컨테이너 컴포넌트를 가져옴
+  const { form,auth,authError } = useSelector(({ auth }) => ({
     form: auth.register,
+    auth:auth.auth,
+    authError:auth.authError
   }));
 
   // 인풋 변경 이벤트 핸들러
@@ -24,8 +26,30 @@ const RegisterForm = () => {
   // 폼 등록 이벤트 핸들러
   const onSubmit = (e) => {
     e.preventDefault();
-    //구현 예정
+    const {username,password,passwordConfirm} = form; // state 에 존재하는 form에 따른 username,password,passwordconfirm key-value를 객체비구조화로 나타냄.
+    if (password!== passwordConfirm){
+      //오류처리 해야함
+      return;
+    }
+    dispatch(register({username,password}));
   };
+
+  //컴포넌트가 처음 렌더링될 때 form 을 초기화함
+  useEffect(()=>{
+    dispatch(initializeForm('register'));},[dispatch]);
+  
+  //회원가입 성공/실패 처리
+  useEffect(()=>{
+    if (authError){
+      console.log('오류 발생');
+      console.log(authError);
+      return;
+    }
+    if (auth){
+      console.log('회원가입 성공');
+      console.log(auth);
+    }
+  },[auth,authError]);
 
   return (
     <AuthForm
--- SpringAndReact/frontend/src/modules/index.js
+++ SpringAndReact/frontend/src/modules/index.js
@@ -1,7 +1,7 @@
 import {combineReducers} from 'redux';
 import {all} from 'redux-saga/effects';
 import auth, {authSaga} from './auth';
-import loading from './Loading';
+import loading from './loading';
 const rootReducer = combineReducers({
     auth,
     loading,
