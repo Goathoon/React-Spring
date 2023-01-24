a4407335275f0c96376d6928bdef82ab3dd1b1f8
SiWon-Heo <calvin0420@g.skku.edu>
2023-01-24 11:15:49 +0900

    handle register form edge cases

--- SpringAndReact/frontend/src/containers/RegisterForm.js
+++ SpringAndReact/frontend/src/containers/RegisterForm.js
@@ -5,10 +5,10 @@ import AuthForm from '../components/auth/AuthForm';
 
 const RegisterForm = () => {
   const dispatch = useDispatch(); //스토어에서 컨테이너 컴포넌트를 가져옴
-  const { form,auth,authError } = useSelector(({ auth }) => ({
+  const { form, auth, authError } = useSelector(({ auth }) => ({
     form: auth.register,
-    auth:auth.auth,
-    authError:auth.authError
+    auth: auth.auth,
+    authError: auth.authError
   }));
 
   // 인풋 변경 이벤트 핸들러
@@ -26,30 +26,48 @@ const RegisterForm = () => {
   // 폼 등록 이벤트 핸들러
   const onSubmit = (e) => {
     e.preventDefault();
-    const {username,password,passwordConfirm} = form; // state 에 존재하는 form에 따른 username,password,passwordconfirm key-value를 객체비구조화로 나타냄.
-    if (password!== passwordConfirm){
-      //오류처리 해야함
+    const { username, password, passwordConfirm } = form; // state 에 존재하는 form에 따른 username,password,passwordconfirm key-value를 객체비구조화로 나타냄.
+
+    // 230124 siwon
+    if (username === '') {
+      window.alert('아이디를 입력하세요.');
       return;
     }
-    dispatch(register({username,password}));
+
+    // 230124 siwon
+    if (password === '') {
+      window.alert('패스워드를 입력하세요.');
+      return;
+    }
+
+    if (password !== passwordConfirm) {
+      // 230124 siwon
+      console.log('오류 발생');
+      window.alert('오류 발생');
+      return;
+    }
+    dispatch(register({ username, password }));
   };
 
   //컴포넌트가 처음 렌더링될 때 form 을 초기화함
-  useEffect(()=>{
-    dispatch(initializeForm('register'));},[dispatch]);
-  
+  useEffect(() => {
+    dispatch(initializeForm('register'));
+  }, [dispatch]);
+
   //회원가입 성공/실패 처리
-  useEffect(()=>{
-    if (authError){
+  useEffect(() => {
+    if (authError) {
+      window.alert('회원가입 처리 도중 오류가 발생했습니다. 서버가 켜져있는지 확인해주세요.');
       console.log('오류 발생');
       console.log(authError);
       return;
     }
-    if (auth){
+    if (auth) {
+      window.alert('환영합니다!');
       console.log('회원가입 성공');
       console.log(auth);
     }
-  },[auth,authError]);
+  }, [auth, authError]);
 
   return (
     <AuthForm
