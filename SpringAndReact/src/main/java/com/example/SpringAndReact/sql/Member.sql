925067e1d4574bd8e5113759ed9727941a2ed043
Goathoon <ownest2@gmail.com>
2023-01-26 00:23:55 +0900

    sql 및 JPA라이브러리 추가

--- SpringAndReact/src/main/java/com/example/SpringAndReact/sql/Member.sql
+++ SpringAndReact/src/main/java/com/example/SpringAndReact/sql/Member.sql
@@ -1,4 +1,6 @@
-CREATE TABLE MEMBERS(
-    ID VARCHAR(50) PRIMARY KEY,
-    PWD VARCHAR(50)
+CREATE TABLE `websitedb`.`member` (
+    `id` INT NULL,
+    `username` VARCHAR(45) NOT NULL,
+    `password` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`id`)
 );
